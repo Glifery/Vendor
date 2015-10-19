64be23f71accbe1f9fd0fa7544fdb9ab025a2b3e
Glifery <glifery@gmail.com>
2015-10-19 17:52:36 +0300

    EntityHiddenTypeBundle example

--- src/Vendor/EntityHiddenTypeBundle/Controller/DefaultController.php
+++ src/Vendor/EntityHiddenTypeBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Vendor\EntityHiddenTypeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Vendor\EntityHiddenTypeBundle\Entity\Entity;
 
 class DefaultController extends Controller
@@ -13,14 +14,17 @@ class DefaultController extends Controller
         $entityRepo = $this->get('doctrine.orm.entity_manager')->getRepository('VendorEntityHiddenTypeBundle:Entity');
         $entities = $entityRepo->findAll();
         /** @var Entity $entity */
-        $entity = $entities[0];
+        $entity = $entities[1];
 
         $formBuilder = $this->createFormBuilder();
         $form = $formBuilder
             ->add('entity', 'entity_hidden', array(
                     'class' => 'VendorEntityHiddenTypeBundle:Entity',
                     'property' => 'name',
-                    'label' => 'EntityHidden'
+                    'data' => $entity,
+                    'invalid_message' => 'The entity does not exist.',
+                    'em' => $this->get('doctrine')->getManager('default'),
+                    'constraints' => new \Symfony\Component\Validator\Constraints\NotBlank()
                 ))
             ->add('name', 'text', array(
                     'label' => 'Text'
@@ -31,13 +35,17 @@ class DefaultController extends Controller
             ->getForm()
         ;
 
-        $form->get('entity')->setData($entity);
-        $form->get('name')->setData($entity->getName());
+//        $form->get('entity')->setData($entity);
+//        $form->get('name')->setData($entity->getName());
 
         $newEntity = null;
+//        $entity = null;
         $form->handleRequest($request);
         if ($form->isValid()) {
             $newEntity = $form->get('entity')->getData();
+//            $name = $form->get('name')->getData();
+//            $entity = $entityRepo->findOneBy(array('name' => $name));
+//            $form->get('entity')->setData($entity);
         }
 
         return $this->render('VendorEntityHiddenTypeBundle:Default:index.html.twig', array(
